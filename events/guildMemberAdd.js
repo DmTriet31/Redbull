a9397617fbe7763e939f89a40d6a6c9062e32910
DmTriet31 <dmtriet.31@gmail.com>
2026-02-24 03:27:31 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -25,7 +25,7 @@ module.exports = (client) => {
     
       const embed = new EmbedBuilder()
   .setColor(0xff4757)
-  .setTitle(`<a:MG_Hi:1447004755351306242> Welcome ${serverName <a:MG_Heart_d1:1470060994968555550>`)
+  .setTitle(`<a:MG_Hi:1447004755351306242> Welcome ${serverName} <a:MG_Heart_d1:1470060994968555550>`)
   .setDescription(
     `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
     `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
